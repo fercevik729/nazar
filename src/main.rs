0dfe9675bb524e431d6656694c7c81748fbb16d4
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-07-05 13:41:00 -0700

    Started CLI using 'clap' package

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
+extern crate clap;
 extern crate pnet;
 
-use pnet::datalink::{self, NetworkInterface};
+use clap::Parser;
 
+use pnet::datalink;
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
-use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::Packet;
 
+#[derive(Parser, Debug)]
+#[command(name = "nazar")]
+#[command(author = "Furkan E. <f.ercevik21@gmail.com>")]
+#[command(version = "0.1.0")]
+#[command(about = "A simple packet sniffing and intrusion detection program")]
+struct Args {
+    // Name of the interface to perform packet sniffing on
+    // If not provided, the program will try to find the first non-loopback
+    // interface
+    #[arg(short, long)]
+    iface: Option<String>,
+
+    // Path to file containing the rules
+    #[arg(short, long, value_name = "FILE")]
+    rules: std::path::PathBuf,
+
+    // Path to directory to output log files
+    // If not provided, the program will write logs in a 'logs' directory in the
+    // current working directory
+    #[arg(short, long, value_name = "DIR")]
+    outlog: Option<std::path::PathBuf>,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    // TODO: handle None case of optional arguments
+
+    println!("{:?}", args);
+
     let interfaces = datalink::interfaces();
     let interface = interfaces
         .into_iter()
