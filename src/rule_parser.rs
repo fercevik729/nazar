add17ab583873540fbcba2c3e690be75787d1270
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-07-08 14:49:44 -0700

    Started making code for BWList more generic

--- src/rule_parser.rs
+++ src/rule_parser.rs
@@ -9,6 +9,12 @@ use serde::Deserialize;
 
 use anyhow::{anyhow, Result};
 
+trait Validate {
+    type Item: Copy;
+
+    fn is_valid(&self, other: Self::Item) -> bool;
+}
+
 // A struct to handle inclusive IP address ranges
 #[derive(Deserialize, PartialEq, Debug)]
 struct IpRange {
@@ -36,10 +42,11 @@ impl IpRange {
         // Bounds check
         if let Some(e) = end_ip {
             if e < begin_ip {
-                return Err(anyhow!(format!(
+                return Err(anyhow!(
                     "end_ip {} must be greater than or equal to begin_ip {}",
-                    e, begin_ip
-                )));
+                    e,
+                    begin_ip
+                ));
             }
         }
 
@@ -48,11 +55,14 @@ impl IpRange {
             end: end_ip,
         })
     }
+}
 
+impl Validate for IpRange {
+    type Item = IpAddr;
     // Checks if an ip address `ip` is in the range of the IpRange
-    fn in_range(&self, ip: IpAddr) -> bool {
+    fn is_valid(&self, other: Self::Item) -> bool {
         // Convert `ip` parameter if it is v4
-        let new_ip = match ip {
+        let new_ip = match other {
             IpAddr::V6(i) => i,
             IpAddr::V4(i) => i.to_ipv6_mapped(),
         };
@@ -119,18 +129,65 @@ mod iprange_tests {
     }
 }
 
-// Enum to represent ip blacklist and whitelist
 #[derive(Deserialize)]
-enum BWList {
-    WhiteList(Vec<IpRange>),
-    BlackList(Vec<IpRange>),
+struct PortRange {
+    begin: i32,
+    end: Option<i32>,
 }
 
-impl BWList {
-    fn is_valid_ip(&self, ip: IpAddr) -> bool {
+impl PortRange {
+    fn new(begin: i32, end: Option<i32>) -> Result<Self> {
+        // Validate the port numbers are within the 0 to 65536 range
+        let valid_ports = 0..65536;
+        if !(valid_ports).contains(&begin) {
+            return Err(anyhow!(
+                "begin port {} must be in the range 0 to 65536",
+                begin
+            ));
+        }
+        match end {
+            Some(e) if !(valid_ports).contains(&e) => {
+                return Err(anyhow!("end port {} must be in the range 0 to 65536", e));
+            }
+            Some(e) if e < begin => {
+                return Err(anyhow!(
+                    "end port {} must be greater than or equal to begin port {}",
+                    e,
+                    begin,
+                ))
+            }
+            _ => Ok(Self { begin, end }),
+        }
+    }
+}
+
+impl Validate for PortRange {
+    type Item = i32;
+
+    fn is_valid(&self, other: Self::Item) -> bool {
+        if let Some(e) = self.end {
+            other >= self.begin && other <= e
+        } else {
+            other == self.begin
+        }
+    }
+}
+
+// Enum to represent blacklist and whitelist
+// for src/dest IP addresses
+// for ports
+// for protocols
+#[derive(Deserialize)]
+enum BWList<T: Validate> {
+    WhiteList(Vec<T>),
+    BlackList(Vec<T>),
+}
+
+impl<T: Validate + PartialEq> BWList<T> {
+    fn is_valid_item(&self, target: T::Item) -> bool {
         match self {
-            BWList::WhiteList(v) => return v.iter().find(|ipr| ipr.in_range(ip)) != None,
-            BWList::BlackList(v) => return v.iter().find(|ipr| ipr.in_range(ip)) == None,
+            BWList::WhiteList(v) => return v.iter().find(|item| item.is_valid(target)) != None,
+            BWList::BlackList(v) => return v.iter().find(|item| item.is_valid(target)) == None,
         }
     }
 }
@@ -146,7 +203,7 @@ mod bwlist_tests {
     use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
     #[test]
-    fn whitelist_tests() -> Result<()> {
+    fn whitelist_ip_tests() -> Result<()> {
         let wl = BWList::WhiteList(vec![
             IpRange::new(
                 IpAddr::V4(Ipv4Addr::new(127, 0, 0, 5)),
@@ -163,18 +220,18 @@ mod bwlist_tests {
             IpRange::new(IpAddr::V4(Ipv4Addr::new(192, 132, 0, 1)), None)?,
         ]);
 
-        assert!(wl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 5))));
-        assert!(!wl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 4))));
-        assert!(wl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(161, 0, 0, 4))));
-        assert!(!wl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(200, 0, 0, 4))));
-        assert!(wl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(181, 0, 0, 4))));
-        assert!(!wl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(192, 132, 0, 4))));
+        assert!(wl.is_valid_item(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 5))));
+        assert!(!wl.is_valid_item(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 4))));
+        assert!(wl.is_valid_item(IpAddr::V4(Ipv4Addr::new(161, 0, 0, 4))));
+        assert!(!wl.is_valid_item(IpAddr::V4(Ipv4Addr::new(200, 0, 0, 4))));
+        assert!(wl.is_valid_item(IpAddr::V4(Ipv4Addr::new(181, 0, 0, 4))));
+        assert!(!wl.is_valid_item(IpAddr::V4(Ipv4Addr::new(192, 132, 0, 4))));
 
         Ok(())
     }
 
     #[test]
-    fn blacklist_tests() -> Result<()> {
+    fn blacklist_ip_tests() -> Result<()> {
         let bl = BWList::BlackList(vec![
             IpRange::new(
                 IpAddr::V4(Ipv4Addr::new(127, 0, 0, 5)),
@@ -191,12 +248,24 @@ mod bwlist_tests {
             IpRange::new(IpAddr::V4(Ipv4Addr::new(192, 132, 0, 1)), None)?,
         ]);
 
-        assert!(!bl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 5))));
-        assert!(bl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 4))));
-        assert!(!bl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(161, 0, 0, 4))));
-        assert!(bl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(200, 0, 0, 4))));
-        assert!(!bl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(181, 0, 0, 4))));
-        assert!(bl.is_valid_ip(IpAddr::V4(Ipv4Addr::new(192, 132, 0, 4))));
+        assert!(!bl.is_valid_item(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 5))));
+        assert!(bl.is_valid_item(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 4))));
+        assert!(!bl.is_valid_item(IpAddr::V4(Ipv4Addr::new(161, 0, 0, 4))));
+        assert!(bl.is_valid_item(IpAddr::V4(Ipv4Addr::new(200, 0, 0, 4))));
+        assert!(!bl.is_valid_item(IpAddr::V4(Ipv4Addr::new(181, 0, 0, 4))));
+        assert!(bl.is_valid_item(IpAddr::V4(Ipv4Addr::new(192, 132, 0, 4))));
+
+        Ok(())
+    }
+
+    #[test]
+    fn whitelist_port_tests() -> Result<()> {
+        let wl = BWList::WhiteList(vec![
+            PortRange::new(80, Some(82)),
+            PortRange::new(90, Some(95)),
+            PortRange::new(103, Some(195)),
+            PortRange::new(202, Some(195)),
+        ]);
 
         Ok(())
     }
@@ -204,5 +273,5 @@ mod bwlist_tests {
 
 #[derive(Deserialize)]
 struct Rules {
-    list: BWList,
+    ip_list: Option<BWList<IpRange>>,
 }
