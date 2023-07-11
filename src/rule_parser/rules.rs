5d67d255c04a74b731571164374bf56eb2a71b5c
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-07-11 14:52:50 -0700

    Starting working on processing HTTP packets

--- src/rule_parser/rules.rs
+++ src/rule_parser/rules.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashMap, net::IpAddr, vec};
-
-use super::{BWList, Deserialize, IpRange, PortRange, Protocol};
+use super::{pnet::packet::Packet, BWList, Deserialize, IpRange, PortRange, Protocol, Result};
+use anyhow::anyhow;
+use pnet::packet::tcp::TcpPacket;
+use std::{collections::HashMap, fmt, net::IpAddr};
 
 #[derive(Deserialize)]
 struct Rules {
@@ -39,14 +40,15 @@ enum ProtocolRule {
 // Enum type to represent transport layer
 // protocol rules
 enum TransportProtocolRule {
-    IcmpRule,
-    Icmpv6Rule,
-    TcpRule,
-    UdpRule,
+    Icmp,
+    Icmpv6,
+    Tcp,
+    Udp,
 }
 
 // Enum type to represent HTTP Methods
-enum HttpMethod {
+#[derive(Deserialize, Debug)]
+pub enum HttpMethod {
     Get,
     Head,
     Post,
@@ -58,16 +60,116 @@ enum HttpMethod {
     Trace,
 }
 
+impl fmt::Display for HttpMethod {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Get => write!(f, "GET"),
+            Self::Head => write!(f, "HEAD"),
+            Self::Post => write!(f, "POST"),
+            Self::Put => write!(f, "PUT"),
+            Self::Patch => write!(f, "PATCH"),
+            Self::Delete => write!(f, "DELETE"),
+            Self::Connection => write!(f, "CONNECTION"),
+            Self::Options => write!(f, "OPTIONS"),
+            Self::Trace => write!(f, "TRACE"),
+        }
+    }
+}
+
+trait ApplicationProtocol {
+    type PacketType<'a>;
+
+    fn process_packet<'a>(&self, packet: Self::PacketType<'a>) -> Result<bool>;
+}
+
+#[derive(Deserialize, Debug)]
+struct HttpRule {
+    method: Option<HttpMethod>,
+    headers_contain: Option<HashMap<String, String>>,
+    path_contains: Option<String>,
+}
+
+impl HttpRule {
+    // Constructor for HTTP Rule
+    fn new(method: HttpMethod, headers: HashMap<String, String>, pattern: String) -> Self {
+        Self {
+            method: Some(method),
+            headers_contain: Some(headers),
+            path_contains: Some(pattern),
+        }
+    }
+}
+
+impl ApplicationProtocol for HttpRule {
+    type PacketType<'a> = TcpPacket<'a>;
+
+    // Assumes that packet is an HTTP packet on port 80 or 8080
+    // Parse it using the httparse library
+    fn process_packet<'a>(&self, packet: Self::PacketType<'a>) -> Result<bool> {
+        // Parse request
+        let body = packet.payload();
+        let mut headers = [httparse::EMPTY_HEADER; 16];
+        let mut req = httparse::Request::new(&mut headers);
+        req.parse(body)?;
+
+        let mut conds = [true; 3];
+
+        // Check the request method
+        match req.method {
+            Some(m) => {
+                if let Some(rm) = &self.method {
+                    if m != rm.to_string() {
+                        conds[0] = false;
+                    }
+                }
+            }
+            None => {
+                return Err(anyhow!(
+                    "Malformed HTTP Request, no method field could be parsed"
+                ))
+            }
+        };
+        // Check the request body
+        match req.path {
+            Some(p) => {
+                if let Some(rp) = &self.path_contains {
+                    if p != rp {
+                        conds[1] = false;
+                    }
+                }
+            }
+            None => {
+                return Err(anyhow!(
+                    "Malformed HTTP request, no path field could be parsed"
+                ))
+            }
+        };
+
+        // Check the headers
+        if let Some(map) = &self.headers_contain {
+            for (header, target) in map.iter() {
+                let value = req.headers.iter().find(|&x| x.name == header);
+                if let Some(found) = value {
+                    conds[2] = found
+                        .value
+                        .windows(target.len())
+                        .any(|window| window == target.as_bytes());
+                    if conds[2] {
+                        break;
+                    }
+                }
+            }
+        }
+
+        Ok(conds.iter().all(|&c| c))
+    }
+}
+
 // Enum type to represent application layer
 // protocol rules
+// TODO: add more application layer protocols later
 enum ApplicationProtocolRule {
-    HttpRule {
-        method: HttpMethod,
-        header_contains: HashMap<String, String>,
-        body_contains: Vec<String>,
-    },
-    DhcpRule,
-    DnsRule,
-    FtpRule,
-    SmtpRule,
+    Http(HttpRule),
+    Dhcp,
+    Dns,
 }
