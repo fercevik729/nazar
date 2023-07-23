a10281a40a074e06dde5adbc8ec5554d219289ec
Furkan Ercevik <ercevik.furkan@gmail.com>
2023-07-23 15:49:41 -0700

    Broke apart rules module

---
+++ src/rule_parser/rules/tcp_rule.rs
@@ -0,0 +1,244 @@
+use super::*;
+
+// Enum representing TCP Options
+#[derive(Deserialize, Debug)]
+enum TcpOption {
+    /// Maximum segment size
+    MSS,
+    /// Window scale
+    WSCALE,
+    /// Selective acknowledgment
+    SACK,
+    /// End of Options list
+    EOL,
+    /// Timestamps
+    TIMESTAMPS,
+    /// No operation
+    NOP,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TcpRule {
+    options: Option<Vec<TcpOption>>,
+    flags: Option<u8>,
+    max_window_size: Option<u16>,
+    max_payload_size: Option<usize>,
+}
+
+impl TcpRule {
+    fn new(
+        options: Option<Vec<TcpOption>>,
+        flags: Option<u8>,
+        max_window_size: Option<u16>,
+        max_payload_size: Option<usize>,
+    ) -> Self {
+        Self {
+            options,
+            flags,
+            max_window_size,
+            max_payload_size,
+        }
+    }
+
+    fn match_opts(&self, x: tcp::TcpOptionPacket) -> bool {
+        // If tcp options are specified in the rule, this function will return true
+        // if at least one of the options are contained in the packet, otherwise it will
+        // return false
+        // If no tcp options are specified in the rule, this function will also return true
+        // since in this case any TCP option is valid
+        if let Some(rule_options) = &self.options {
+            return rule_options.iter().any(|ro| match ro {
+                TcpOption::MSS => x.get_number() == tcp::TcpOptionNumbers::MSS,
+                TcpOption::EOL => x.get_number() == tcp::TcpOptionNumbers::EOL,
+                TcpOption::NOP => x.get_number() == tcp::TcpOptionNumbers::NOP,
+                TcpOption::SACK => x.get_number() == tcp::TcpOptionNumbers::SACK,
+                TcpOption::WSCALE => x.get_number() == tcp::TcpOptionNumbers::WSCALE,
+                TcpOption::TIMESTAMPS => x.get_number() == tcp::TcpOptionNumbers::TIMESTAMPS,
+            });
+        }
+
+        true
+    }
+}
+
+impl ProcessPacket for TcpRule {
+    fn process(&self, body: &[u8]) -> Result<bool> {
+        // Assumes that the packet is some kind of TCP Packet though not necessarily a valid one
+        // The function parses the byte slice into a TcpPacket struct using the
+        // pnet library. It returns an error if something went wrong during parsing
+        //
+        // All parameters in the Rule struct are optional, and if not explicitly provided,
+        // this function will skip those parameters
+        //
+        // For a request to return 'true' indicating that it matches the Rule struct provided,
+        // it must match all the fields and *at least one* of any subfields.
+        // Try to make a TCP Packet
+        if let Some(tcp_packet) = TcpPacket::new(body) {
+            // Check the options
+            if !tcp_packet.get_options_iter().any(|x| self.match_opts(x)) {
+                return Ok(false);
+            }
+
+            // Check the window size
+            if let Some(max_window_size) = self.max_window_size {
+                if max_window_size < tcp_packet.get_window() {
+                    return Ok(false);
+                }
+            }
+
+            // Check the flags
+            if let Some(flags) = self.flags {
+                return Ok((flags & tcp_packet.get_flags()) > 0);
+            }
+
+            // Check the payload size
+            if let Some(max_payload_size) = self.max_payload_size {
+                if max_payload_size < tcp_packet.packet_size() {
+                    return Ok(false);
+                }
+            }
+
+            Ok(true)
+        } else {
+            Err(anyhow!("Unable to parse TCP Packet"))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pnet::packet::{tcp::MutableTcpPacket, Packet};
+
+    use super::*;
+
+    const TCP_PACKET_LEN: usize = 30;
+    const TCP_PACKET_OFS: u8 = 10;
+
+    #[test]
+    fn test_tcp_process_packet_1() -> Result<()> {
+        // Tests for TCP options in particular
+        // 1 option - matches
+        let rule = TcpRule::new(Some(vec![TcpOption::NOP]), None, None, None);
+        let buff = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet = MutableTcpPacket::owned(buff).unwrap();
+        tcp_packet.set_data_offset(TCP_PACKET_OFS); // TCP header data offset
+        tcp_packet.set_flags(tcp::TcpFlags::SYN); // TCP header flags
+        tcp_packet.set_options(&[tcp::TcpOption::nop()]);
+
+        assert!(rule.process(tcp_packet.packet())?);
+
+        // 2 options - at least 1 matches
+        let buff_2 = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet_2 = MutableTcpPacket::owned(buff_2).unwrap();
+        tcp_packet_2.set_data_offset(TCP_PACKET_OFS); // TCP header data offset
+        tcp_packet_2.set_flags(tcp::TcpFlags::SYN); // TCP header flags
+        tcp_packet_2.set_options(&[tcp::TcpOption::nop(), tcp::TcpOption::sack_perm()]);
+
+        assert!(rule.process(tcp_packet_2.packet())?);
+
+        // 2 options - don't match
+        let buff_3 = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet_3 = MutableTcpPacket::owned(buff_3).unwrap();
+        tcp_packet_3.set_data_offset(TCP_PACKET_OFS); // TCP header data offset
+        tcp_packet_3.set_flags(tcp::TcpFlags::SYN); // TCP header flags
+        tcp_packet_3.set_options(&[tcp::TcpOption::mss(3), tcp::TcpOption::sack_perm()]);
+
+        assert!(!rule.process(tcp_packet_3.packet())?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_tcp_process_packet_2() -> Result<()> {
+        // Tests for TCP Flags in particular
+        // 1 flag - matches
+        let rule = TcpRule::new(Some(vec![TcpOption::MSS]), Some(0x02), None, None);
+        let buff = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet = MutableTcpPacket::owned(buff).unwrap();
+        tcp_packet.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet.set_flags(tcp::TcpFlags::SYN);
+        tcp_packet.set_options(&[tcp::TcpOption::mss(10)]);
+
+        assert!(rule.process(tcp_packet.packet())?);
+
+        // 1 flag - doesn't match
+        let buff_2 = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet_2 = MutableTcpPacket::owned(buff_2).unwrap();
+        tcp_packet_2.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet_2.set_flags(tcp::TcpFlags::ACK);
+        tcp_packet_2.set_options(&[tcp::TcpOption::mss(10)]);
+
+        assert!(!rule.process(tcp_packet_2.packet())?);
+
+        // 3 flags - at least 1 match
+        let buff_3 = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet_3 = MutableTcpPacket::owned(buff_3).unwrap();
+        tcp_packet_3.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet_3.set_flags(tcp::TcpFlags::ACK | tcp::TcpFlags::SYN | tcp::TcpFlags::URG);
+        tcp_packet_3.set_options(&[tcp::TcpOption::mss(10)]);
+
+        assert!(rule.process(tcp_packet_3.packet())?);
+
+        // 3 flags - none match
+        let buff_4 = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet_4 = MutableTcpPacket::owned(buff_4).unwrap();
+        tcp_packet_4.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet_4.set_flags(tcp::TcpFlags::ACK | tcp::TcpFlags::ECE | tcp::TcpFlags::URG);
+        tcp_packet_4.set_options(&[tcp::TcpOption::mss(10)]);
+
+        assert!(!rule.process(tcp_packet_4.packet())?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_tcp_process_packet_3() -> Result<()> {
+        // Test for maximum window packet_size
+        // Should pass: < 1024
+        let rule = TcpRule::new(Some(vec![TcpOption::MSS]), Some(0x02), Some(1024), None);
+        let buff = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet = MutableTcpPacket::owned(buff).unwrap();
+        tcp_packet.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet.set_flags(tcp::TcpFlags::SYN);
+        tcp_packet.set_options(&[tcp::TcpOption::mss(10)]);
+        tcp_packet.set_window(512);
+
+        assert!(rule.process(tcp_packet.packet())?);
+
+        // Should fail: > 1024
+        let buff_2 = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet_2 = MutableTcpPacket::owned(buff_2).unwrap();
+        tcp_packet_2.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet_2.set_flags(tcp::TcpFlags::SYN);
+        tcp_packet_2.set_options(&[tcp::TcpOption::mss(10)]);
+        tcp_packet_2.set_window(2048);
+
+        assert!(!rule.process(tcp_packet_2.packet())?);
+
+        // Should pass: == 1024
+        let buff_3 = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet_3 = MutableTcpPacket::owned(buff_3).unwrap();
+        tcp_packet_3.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet_3.set_flags(tcp::TcpFlags::SYN);
+        tcp_packet_3.set_options(&[tcp::TcpOption::mss(10)]);
+        tcp_packet_3.set_window(1024);
+
+        assert!(rule.process(tcp_packet_3.packet())?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_tcp_process_packet_4() -> Result<()> {
+        // Test for maximum payload size
+        // Should pass: <= 30
+        let rule = TcpRule::new(Some(vec![TcpOption::MSS]), Some(0x02), Some(1024), Some(30));
+        let buff = vec![0u8; TCP_PACKET_LEN];
+        let mut tcp_packet = MutableTcpPacket::owned(buff).unwrap();
+        tcp_packet.set_data_offset(TCP_PACKET_OFS);
+        tcp_packet.set_flags(tcp::TcpFlags::SYN);
+        tcp_packet.set_options(&[tcp::TcpOption::mss(10)]);
+        tcp_packet.set_window(512);
+
+        assert!(rule.process(tcp_packet.packet())?);
+        Ok(())
+    }
+}
---
+++ src/rule_parser/rules/udp_rule.rs
@@ -0,0 +1,413 @@
+use super::*;
+
+// Enum representing next level protocols on top of UDP
+// DNS is excluded because it has its own separate rule
+// implementation
+#[derive(Deserialize, Debug)]
+enum UdpNextLevel {
+    // Domain Name System
+    DNS,
+    // Dynamic Host Configuration Protocol
+    DHCP,
+    // Simple Network Management Protocol
+    SNMP,
+    // Sessions Initiation Protocol
+    SIP,
+    // Real-Time Transport Protocol
+    RTP,
+}
+
+// Struct representing a UDP rule
+#[derive(Deserialize, Debug)]
+pub struct UdpRule {
+    next_protocol: Option<UdpNextLevel>,
+    max_packet_size: Option<usize>,
+}
+
+impl UdpRule {
+    fn new(next_protocol: Option<UdpNextLevel>, max_packet_size: Option<usize>) -> Self {
+        Self {
+            next_protocol,
+            max_packet_size,
+        }
+    }
+}
+
+// A function to determine if a UDP Packet is a DNS Packet
+pub fn is_dns(payload: &[u8]) -> bool {
+    if payload.len() < 12 {
+        return false;
+    }
+
+    let dns_header = &payload[..12];
+    dns_header
+        == [
+            0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ]
+}
+
+// DHCP Constants
+const DHCP_MAGIC_COOKIE: u32 = 0x63825363;
+const DHCP_OPTION_MSG_TYPE: u8 = 53;
+
+const DHCP_DISCOVER: u8 = 1;
+const DHCP_OFFER: u8 = 2;
+const DHCP_REQUEST: u8 = 3;
+const DHCP_ACK: u8 = 5;
+
+// A function to determine if a byte slice is a DHCP Packet
+pub fn is_dhcp(payload: &[u8]) -> bool {
+    // Check if it is long enough
+    if payload.len() < 240 {
+        return false;
+    }
+
+    // Not a valid DHCP Packet if it doesn't contain the magic cookie
+    let magic_cookie = u32::from_be_bytes([payload[236], payload[237], payload[238], payload[239]]);
+    if magic_cookie != DHCP_MAGIC_COOKIE {
+        return false;
+    }
+
+    let mut idx = 240;
+    while idx + 1 < payload.len() {
+        let opt_code = payload[idx];
+        if opt_code == DHCP_OPTION_MSG_TYPE {
+            // Found a DHCP Option
+            let opt_len = payload[idx + 1];
+            if idx + 1 + (opt_len as usize) < payload.len() {
+                // Match agains the option in the payload
+                match payload[idx + 2] {
+                    // Only valid DHCP Packet Types are recognized
+                    DHCP_DISCOVER | DHCP_ACK | DHCP_OFFER | DHCP_REQUEST => return true,
+                    // Anything else returns false
+                    _ => return false,
+                }
+            }
+        }
+
+        idx += 2 + (payload[idx + 1] as usize)
+    }
+
+    false
+}
+
+// SNMP Constants
+const SNMP_VERSION_1: u8 = 0;
+const SNMP_VERSION_2C: u8 = 1;
+const SNMP_VERSION_3: u8 = 3;
+
+const SNMP_GET_REQUEST: u8 = 0xA0;
+const SNMP_GET_NEXT_REQUEST: u8 = 0xA1;
+const SNMP_GET_RESPONSE: u8 = 0xA2;
+const SNMP_SET_REQUEST: u8 = 0xA3;
+
+// Function to determine if a UDP Packet is a SNMP Packet
+pub fn is_snmp(payload: &[u8]) -> bool {
+    // Check payload size
+    if payload.len() < 6 {
+        return false;
+    }
+
+    // Check version
+    let snmp_vers = payload[0];
+    match snmp_vers {
+        SNMP_VERSION_1 | SNMP_VERSION_2C | SNMP_VERSION_3 => {
+            // Check the message type
+            let message_type = payload[1];
+            match message_type {
+                SNMP_GET_REQUEST | SNMP_SET_REQUEST | SNMP_GET_RESPONSE | SNMP_GET_NEXT_REQUEST => {
+                    true
+                }
+                // Unsupported message type
+                _ => false,
+            }
+        }
+        _ => false,
+    }
+}
+
+// Function to determine if a UDP packet is a SIP Packet
+pub fn is_sip(payload: &[u8]) -> bool {
+    // Check payload size
+    if payload.len() < 3 {
+        return false;
+    }
+
+    // Check if the packet starts with the expected header:
+    // INV or SIP in binary
+    match &payload[..3] {
+        b"INV" | b"SIP" => true,
+        _ => false,
+    }
+}
+
+// Function to determine if a UDP packet is a RTP PacketSize
+fn is_rtp(payload: &[u8]) -> bool {
+    // Check the payload size
+    if payload.len() < 12 {
+        return false;
+    }
+
+    // Check the version number by determining if the two MSB's == 2
+    (payload[0] >> 6) == 2
+}
+
+impl ProcessPacket for UdpRule {
+    fn process(&self, body: &[u8]) -> Result<bool> {
+        if let Some(udp_packet) = UdpPacket::new(body) {
+            // Check the next level protocols using the previously defined helper functions
+            if let Some(next_protocol) = &self.next_protocol {
+                let payload = udp_packet.payload();
+                match next_protocol {
+                    UdpNextLevel::DNS => {
+                        if !is_dns(payload) {
+                            return Ok(false);
+                        }
+                    }
+                    UdpNextLevel::DHCP => {
+                        if !is_dhcp(payload) {
+                            return Ok(false);
+                        }
+                    }
+                    UdpNextLevel::RTP => {
+                        if !is_rtp(payload) {
+                            return Ok(false);
+                        }
+                    }
+                    UdpNextLevel::SIP => {
+                        if !is_sip(payload) {
+                            return Ok(false);
+                        }
+                    }
+                    UdpNextLevel::SNMP => {
+                        if !is_snmp(payload) {
+                            return Ok(false);
+                        }
+                    }
+                }
+            }
+
+            if let Some(max_packet_size) = self.max_packet_size {
+                // Check the packet size
+                // Return false if the size of the udp packet is less than then
+                // maximum packet size
+                if max_packet_size < udp_packet.packet_size() {
+                    return Ok(false);
+                }
+            }
+
+            Ok(true)
+        } else {
+            Err(anyhow!("Unable to parse UDP packet."))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use pnet::packet::{udp::MutableUdpPacket, Packet};
+
+    use super::*;
+
+    #[test]
+    fn test_is_dns() {
+        // Test the is_dns helper function
+        //
+        // Check the length
+        let short_payload = &[0u8; 11];
+        assert!(!is_dns(short_payload));
+
+        // Check the header
+        let bad_header_payload = &[0u8; 12];
+        assert!(!is_dns(bad_header_payload));
+
+        let valid_payload = &[
+            0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+        assert!(is_dns(valid_payload));
+    }
+
+    #[test]
+    fn test_is_dhcp() {
+        // Test the is_dhcp helper function
+        //
+        // Check the length
+        let short = [0u8; 239];
+        assert!(!is_dhcp(&short));
+
+        // Check the cookie
+        let bad_header = [0u8; 241];
+        assert!(!is_dhcp(&bad_header));
+
+        // Check the options
+        let mut vec1 = vec![0u8; 236];
+        let vec2: Vec<u8> = vec![
+            // Magic Cookie: 0x63825363
+            0x63, 0x82, 0x53, 0x63, // DHCP Option: DHCP Message Type (53)
+            0x35, 0x01, 0x04, // Invalid option
+            // End Option (255)
+            0xFF,
+        ];
+        vec1.extend(vec2);
+
+        let invalid_opt: [u8; 244] = {
+            let mut array_data: [u8; 244] = [0u8; 244];
+            let len = vec1.len();
+            array_data[..len].copy_from_slice(&vec1);
+            array_data
+        };
+
+        assert!(!is_dhcp(&invalid_opt));
+
+        // Valid payload
+        let mut vec_valid = vec![0u8; 236];
+        let vec2_valid: Vec<u8> = vec![
+            // Magic Cookie: 0x63825363
+            0x63, 0x82, 0x53, 0x63, // DHCP Option: DHCP Message Type (53)
+            0x35, 0x01, 0x01, // DHCP Message Type: Discover (1)
+            // End Option (255)
+            0xFF,
+        ];
+        vec_valid.extend(vec2_valid);
+        let valid_payload: [u8; 244] = {
+            let mut array_data: [u8; 244] = [0u8; 244];
+            let len = vec_valid.len();
+            array_data[..len].copy_from_slice(&vec_valid);
+            array_data
+        };
+        assert!(is_dhcp(&valid_payload))
+    }
+
+    #[test]
+    fn test_is_snmp() {
+        // Test the is_snmp helper function
+        //
+        // Test length
+        let short = [0u8; 5];
+        assert!(!is_snmp(&short));
+
+        // Test version number
+        let invalid_version = [0x05, 0xA0, 0x00, 0x00, 0x00, 0x00];
+        assert!(!is_snmp(&invalid_version));
+
+        // Test invalid type
+        let invalid_type = [0x00, 0xA4, 0x00, 0x00, 0x00, 0x00];
+        assert!(!is_snmp(&invalid_type));
+
+        // Valid
+        let valid = [0x00, 0xA0, 0x00, 0x00, 0x00, 0x00];
+        assert!(is_snmp(&valid))
+    }
+
+    #[test]
+    fn test_is_sip() {
+        // Test the is_sip helper function
+        //
+        // Test length
+        let short = [0u8];
+        assert!(!is_sip(&short));
+
+        // Test header
+        let invalid_header = b"SJP";
+        assert!(!is_sip(invalid_header));
+
+        let invalid_header_2 = b"INW";
+        assert!(!is_sip(invalid_header_2));
+
+        let valid_1 = b"SIP";
+        assert!(is_sip(valid_1));
+
+        let valid_2 = b"INV";
+        assert!(is_sip(valid_2));
+    }
+
+    #[test]
+    fn test_is_rtp() {
+        // Test the is_rtp helper function
+        //
+        // Test length
+        let short = [0u8; 11];
+        assert!(!is_rtp(&short));
+
+        let invalid_header = [0u8; 12];
+        assert!(!is_rtp(&invalid_header));
+
+        let mut valid = [0u8; 15];
+        valid[0] = 0x80;
+        assert!(is_rtp(&valid));
+    }
+
+    // Test process function
+    #[test]
+    fn test_udp_packet_1() -> Result<()> {
+        let mut body = [0u8; 50];
+        let mut udp_packet = MutableUdpPacket::new(&mut body).unwrap();
+        let dns_payload = [
+            0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+        udp_packet.set_length(130);
+        udp_packet.set_payload(&dns_payload);
+
+        let rule = UdpRule::new(Some(UdpNextLevel::DNS), Some(128));
+        assert!(rule.process(udp_packet.packet())?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_udp_packet_2() -> Result<()> {
+        let mut body = [0u8; 50];
+        let mut udp_packet = MutableUdpPacket::new(&mut body).unwrap();
+        let sip_payload = b"SIP";
+        udp_packet.set_payload(sip_payload);
+
+        let rule = UdpRule::new(Some(UdpNextLevel::DHCP), None);
+        assert!(!rule.process(udp_packet.packet())?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_udp_packet_3() -> Result<()> {
+        let mut body = [0u8; 50];
+        let mut udp_packet = MutableUdpPacket::new(&mut body).unwrap();
+        let sip_payload = b"SIP";
+        udp_packet.set_payload(sip_payload);
+
+        let rule = UdpRule::new(Some(UdpNextLevel::SIP), Some(6));
+        assert!(!rule.process(udp_packet.packet())?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_udp_packet_4() -> Result<()> {
+        let mut body = [0u8; 300];
+        let mut udp_packet = MutableUdpPacket::new(&mut body).unwrap();
+
+        // DHCP payload
+        let mut vec1 = vec![0u8; 236];
+        let vec2: Vec<u8> = vec![
+            // Magic Cookie: 0x63825363
+            0x63, 0x82, 0x53, 0x63, // DHCP Option: DHCP Message Type (53)
+            0x35, 0x01, 0x01, // DHCP Message Type: Discover (1)
+            // End Option (255)
+            0xFF,
+        ];
+        vec1.extend(vec2);
+        let dhcp_payload: [u8; 244] = {
+            let mut array_data: [u8; 244] = [0u8; 244];
+            let len = vec1.len();
+            array_data[..len].copy_from_slice(&vec1);
+            array_data
+        };
+        udp_packet.set_length(300);
+        udp_packet.set_payload(&dhcp_payload);
+
+        let rule = UdpRule::new(Some(UdpNextLevel::DHCP), Some(100));
+        assert!(rule.process(udp_packet.packet())?);
+
+        Ok(())
+    }
+}
